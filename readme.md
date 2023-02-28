ce916e74a521775019749285ab6fed45e81b46cf
Thuy Hien Tran Nguyen <trhien541@gmail.com>
2023-02-28 11:51:58 +0200

    new version with heroku

---
+++ readme.md
@@ -0,0 +1,20 @@
+# WEATHER_FORECAST
+------------------------------------------------------------------------------
+### SIMPLE WEATHER FORECAST APP DEPLOYED ON HEROKU
+#### DEMO APP
+![PIC1](https://user-images.githubusercontent.com/49017322/221565215-7fb2c69a-197d-4d26-8e1b-7321613de876.png)
+
+</br></br>
+#### PROJECT PREPARATIONS:
+</br>
+
+##### REACT INSTALLATION :   <i>[DOCUMENTATION](https://kinsta.com/blog/how-to-install-node-js/)</i>
+</br>
+
+##### 1ST SETUP FOR HEROKU:   <i>[DOCUMENTSTION](https://devcenter.heroku.com/start)</i>
+</br>
+
+##### REACT DEPLOYMENT ON HEROKU:   <i>[DOCUMENTATION](https://blog.heroku.com/deploying-react-with-zero-configuration)</i>
+
+</br></br>
+#### Enviroment testing:[read this](./README.md)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([])
+  const [city, setCity] = useState("")
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=65d7d5927f7d6f1076c127c58115e9ee&units=metric`
+  
+  const searchFunction = (e) => {
+    if (e.key === 'Enter'){    
+      axios.get(url).then((res) => {
+      setData(res.data)
+      console.log(res.data)
+    })
+      setCity('')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <input
+          value={city}
+          onChange={event => setCity(event.target.value)}
+          onKeyDown={searchFunction}
+          placeholder='Enter Location'
+          type="text" />
+      </div>
+
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <h1 className="bold">{data.name}</h1>
+          </div>
+          <div className="temp">
+            {data.main ? <h1 className="bold">{data.main.temp.toFixed()}°C</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        
+        {data.name !== undefined &&
+        <div className="bottom">
+          <div className="feels">
+            {data.main ? <p className="bold">{data.main.temp.toFixed()}°C</p> : null}
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+            <p>Wind Speed</p>
+          </div>
+        </div>
+        }
+      
+      </div>
     </div>
   );
 }
